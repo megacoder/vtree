32076d5a9814d803c8caf0f369a9c8be9071bbff
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2012-06-21 10:30:29 -0500

    Use only autoreconf instead of each individual tool.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-if test -x /usr/bin/autoreconf; then
-	echo "Using autoreconf(1)..."
-	/usr/bin/autoreconf -fvim
-else
-	echo "Using individual tools..."
-	aclocal && autoheader && autoconf && automake --add-missing --copy
-fi
+autoreconf -fsvim
